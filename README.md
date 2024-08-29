fcf2e368edcea34f8ca47147493e4b4c187686df
Marco Silva <arthurpbs@gmail.com>
2024-08-29 15:44:46 -0300

    initial

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Mojo::Role::Retry - It's new $module
+
+# SYNOPSIS
+
+    use Mojo::Role::Retry;
+
+# DESCRIPTION
+
+Mojo::Role::Retry is ...
+
+# LICENSE
+
+Copyright (C) Marco Silva.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Marco Silva <arthurpbs@gmail.com>
---
+++ lib/Mojo/Role/Retry.pm
@@ -0,0 +1,39 @@
+package Mojo::Role::Retry;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojo::Role::Retry - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mojo::Role::Retry;
+
+=head1 DESCRIPTION
+
+Mojo::Role::Retry is ...
+
+=head1 LICENSE
+
+Copyright (C) Marco Silva.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Marco Silva E<lt>arthurpbs@gmail.comE<gt>
+
+=cut
+
